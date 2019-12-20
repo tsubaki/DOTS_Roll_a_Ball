732e276b3b63dbbce552af5d67c4f9b170ba07a4
tsubaki <ruitomo.ex@gmail.com>
2019-12-20 14:50:59 +0900

    「カウントダウン後にゲームスタート」の動きを追加

--- Assets/Scripts/Systems/MoveBallSystem.cs
+++ Assets/Scripts/Systems/MoveBallSystem.cs
@@ -4,11 +4,18 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics;
 using UnityEngine;
+using static Unity.Entities.ComponentType;
 
 // InputManagerのVertical とHorizontal の情報に合わせてボールを動かす。
 // ボールはUnity.Physics上で動作。
 // カメラの向きは考慮しない。
 public class MoveBallSystem : JobComponentSystem {
+
+    protected override void OnCreate()
+    {
+        RequireForUpdate(GetEntityQuery(ReadOnly<Controlable>()));
+    }
+
     protected override JobHandle OnUpdate (JobHandle inputDeps) {
         var DeltaTime = Time.DeltaTime;
         var duration = new float3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
