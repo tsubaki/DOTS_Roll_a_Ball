ea3a3ebb65796debbfffc78d5db5b0e36f5e1965
tsubaki <ruitomo.ex@gmail.com>
2019-12-20 18:24:06 +0900

    ゲームの進行をGameManagerが管理

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
+// ゲームの進行管理を行う
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TMPro.TextMeshProUGUI label;
 
+    Entity controlable, gamestate;
+
     IEnumerator Start()
     {
+        var manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        gamestate = manager.CreateEntity(typeof(GameState));
+        label.enabled = true;
+
         for(int i=3; i>0; i--)
         {
             label.text = i.ToString();
             yield return new WaitForSeconds(1);
         }
+
+        controlable = manager.CreateEntity(typeof(Controlable));
+        label.text = "GO";
+
+        yield return new WaitForSeconds(1);
         label.enabled = false;
 
-        World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntity(typeof(Controlable));
+
+        yield return new WaitWhile((()=>manager.GetComponentData<GameState>(gamestate).ItemCount != 0));
+
+        label.enabled = true;
+        label.text = "WIN";
+        manager.DestroyEntity(controlable);
     }
 }
---
+++ Assets/Scripts/Systems/GameStatusSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class GameStatusSystem : JobComponentSystem
+{
+    EntityQuery query;
+
+    protected override void OnCreate()
+    {
+        query = GetEntityQuery(typeof(Cube));
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var count = query.CalculateEntityCount();
+
+        Entities.ForEach((ref GameState state)=> state.ItemCount = count).Run();
+
+        return inputDeps;
+    }
+}
