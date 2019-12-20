947b33d753889a5c2f8bab284e8154e8c23d805c
tsubaki <ruitomo.ex@gmail.com>
2019-12-20 14:51:16 +0900

    カウントダウン後にゲーム開始を追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TMPro.TextMeshProUGUI label;
+
+    IEnumerator Start()
+    {
+        for(int i=3; i>0; i--)
+        {
+            label.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+        label.enabled = false;
+
+        World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntity(typeof(Controlable));
+    }
+}
