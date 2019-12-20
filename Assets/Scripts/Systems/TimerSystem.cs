a7286f57ef3201446409499656e057e00eea7970
tsubaki <ruitomo.ex@gmail.com>
2019-12-20 19:26:51 +0900

    add timer UI

---
+++ Assets/Scripts/Systems/TimerSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+public class TimerSystem : JobComponentSystem
+{
+
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<Controlable>();
+    }
+
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities
+            .WithoutBurst()
+            .ForEach((ref Timer timer)=>{
+                timer.Value -= Time.DeltaTime;
+            }).Run();
+
+        Entities
+            .WithoutBurst()
+            .ForEach((TMPro.TextMeshPro label, ref Timer timer)=>{
+                label.text = timer.Value.ToString("000.00");
+            }).Run();
+
+
+        return inputDeps;
+    }
+}
